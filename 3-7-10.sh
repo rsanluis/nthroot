4bf80d8536487065ab1b9ecef1ad8fd8fc86b17b
Roland San Luis <rsanluis@gmail.com>
2023-10-18 12:01:08 -0400

    adding limit rule when x is 1 for 0th root

--- 3-7-10.sh
+++ 3-7-10.sh
@@ -8,5 +8,10 @@ for p in {0..21}; do
   n=$p
   a=$(echo "10^$p"|bc)
   printf "n=%2s nth root of (10^%2s=%28s) is " $n $p $a
-  awk "BEGIN { print $a ** (1 / $n) }"
+  if [[ $p -eq 0 ]]
+  then
+    echo "1"
+  else
+    awk "BEGIN { print $a ** (1 / $n) }"
+  fi
 done
